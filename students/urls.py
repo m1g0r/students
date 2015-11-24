20a7bcd2dd3bc5a0749eb090a4b5d49831017443
m1g0r <m1g0rm1g0r@gmail.com>
2015-11-24 10:23:33 +0200

    add base.html end imitation pattern

--- students/urls.py
+++ students/urls.py
@@ -19,3 +19,21 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+urlpatterns = patterns('',
+# Students urls
+	url(r'^$', 'student.views.student_list', name='home'),
+	url(r'^students/add/$', 'student.views.student_add', name='students_add'),
+	url(r'^students/(?P<sid>\d+)/edit/$', 'student.views.student_edit', name='students_edit'),
+	url(r'^students/(?P<sid>\d+)/delete/$', 'student.views.student_delete', name='students_delete'),
+# Groups urls
+	url(r'^groups/$', 'student.views.groups_list', name='groups'),
+	url(r'^groups/add/$', 'student.views.groups_add', name='groups_add'),
+	url(r'^groups/(?P<gid>\d+)/edit/$', 'student.views.groups_edit', name='groups_edit'),
+	url(r'^groups/(?P<gid>\d+)/delete/$', 'student.views.groups_delete', name='groups_delete'),
+
+# admin
+	url(r'^admin/', include(admin.site.urls)),
+)
