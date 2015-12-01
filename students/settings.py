5e66e74104ae4db6f67c6008c82d1a912aa1132a
m1g0r <m1g0rm1g0r@gmail.com>
2015-12-01 14:45:28 +0200

    add context_processors

--- students/settings.py
+++ students/settings.py
@@ -64,6 +64,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.core.context_processors.request',
+                #'studentsdb.context_processors.students_proc',
             ],
         },
     },
